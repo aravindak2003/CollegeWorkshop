c38551c1048cc7094447cc05d9023ade1c7941ae
Sankararaman Vedagiri <shnkar007@hotmail.com>
2023-08-05 12:37:16 +0530

    Added logic for add student

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -6,6 +6,8 @@ public class Student {
     private String studentId;
     private String name;
     private String course;
+    private String startDate;
+    private String endDate;
 
     public String getStudentId() {
         return studentId;
@@ -15,6 +17,35 @@ public class Student {
         this.studentId = studentId;
     }
 
-    private Date startDate;
-    private Date endDate;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
 }
--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -4,13 +4,26 @@ import model.Student;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 public class StudentService {
     public static void main(String... args){
-
+      StudentService ss= new StudentService();
+      ss.addStudent();
+      ss.getStudents();
     }
 
     public void addStudent(){
+        Student student = new Student();
+        List<Student> studentList = new ArrayList<Student>();
+        student.setStudentId("001");
+        student.setName("name");
+        student.setCourse("MCA");
+        student.setStartDate("01/08/2023");
+        student.setEndDate("08/01/2026");
+        studentList.add(student);
+
+
 
     }
 
